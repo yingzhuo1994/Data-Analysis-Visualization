90757d2160c585a61714927182b21a0aaef0eb5b
Ying Zhuo <zhuoying1994@gmail.com>
2021-08-14 16:27:47 -0500

    Renew graph labels

--- review_analysis/1-av-rat-day.py
+++ review_analysis/1-av-rat-day.py
@@ -24,10 +24,10 @@ chart_def = """
         reversed: false,
         title: {
             enabled: true,
-            text: 'Altitude'
+            text: 'Date'
         },
         labels: {
-            format: '{value} km'
+            format: '{value}'
         },
         accessibility: {
             rangeDescription: 'Range: 0 to 80 km.'
@@ -37,7 +37,7 @@ chart_def = """
     },
     yAxis: {
         title: {
-            text: 'Temperature'
+            text: 'Average Rating'
         },
         labels: {
             format: '{value}°'
@@ -52,7 +52,7 @@ chart_def = """
     },
     tooltip: {
         headerFormat: '<b>{series.name}</b><br/>',
-        pointFormat: '{point.x} km: {point.y}°C'
+        pointFormat: '{point.x}: {point.y}'
     },
     plotOptions: {
         spline: {
@@ -62,7 +62,7 @@ chart_def = """
         }
     },
     series: [{
-        name: 'Temperature',
+        name: 'Averating Rating',
         data: [[0, 15], [10, -50], [20, -56.5], [30, -46.5], [40, -22.1],
             [50, -2.5], [60, -27.7], [70, -55.7], [80, -76.5]]
     }]
