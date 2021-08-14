2ea23542e7aebb9bb0c757d72b8176833c190f8f
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-14 15:41:45 -0500

    Add two files

---
+++ review_analysis/0-simple-app.py
@@ -0,0 +1,9 @@
+import justpy as jp
+
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp, text="Analsis of Course Reviews", classes="text-h3 text-center q-pa-md")
+    p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+    return wp
+
+jp.justpy(app)
